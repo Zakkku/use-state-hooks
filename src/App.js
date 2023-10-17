8c6e0c32dc7ec965db1e2d29ee7096710e383725
Zach <wakuwakuzakku@gmail.com>
2023-10-17 15:10:32 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+  const [number, setNumber] = React.useState(0);
+  const [textValue, setTextValue] = React.useState('');
+  // use a boolean to move through stages of a multi-stage form
+  const [formStage, setFormStage] = React.useState(0);
+  //
+  const [names, setNames] = React.useState(['Zach', 'An'])
+
+  let content;
+  if (formStage === 0) {
+    content = (
+      <>
+      <h1>Let's use state: Step 1</h1>
+      
+        
+
+        <input onChange={e => setTextValue(e.target.value)} type="text"></input>
+        
+        
+        <button onClick={() => setFormStage(formStage + 1) }>Next</button>
+        {textValue.length > 10 && <div className="error-message">Input too long</div>}
+        <p>{textValue}</p>
+        </>
+    )
+  }
+
+  if (formStage === 1) {
+    content = (
+      <>
+      <h1>Let's use state: Step 2</h1>
+      
+        <h2>{number}</h2>
+        
+        <button onClick={() => setFormStage(formStage - 1) }>Previous</button>
+
+        <button onClick={() => setNumber(number + 1)}>
+          +
+        </button>
+
+        <button onClick={() => setFormStage(formStage + 1) }>Next</button>
+        
+        </>
+    )
+  }
+
+  if (formStage === 2) {
+    content = (
+      <>
+      <h1>Let's use state: Step 3</h1>
+      
+      <button onClick={() => setFormStage(formStage - 1) }>Previous</button>
+      <button onClick={() => setNames([...names, "Yamaguchi"])}>Add name</button>
+       
+       
+        {
+        names.map(name => <div>{name}</div>)
+      }
+        </>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        {content}
     </div>
   );
 }
